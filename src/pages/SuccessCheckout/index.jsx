c6ae879e811507a49584f4a98f33f276332031a9
hafidz491 <hafidzhidayatulloh491@gmail.com>
2025-01-13 00:09:29 +0700

    fixing bugs checkout page

--- src/pages/SuccessCheckout/index.jsx
+++ src/pages/SuccessCheckout/index.jsx
@@ -1,39 +1,25 @@
 import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 
-const SuccessCheckoutPage = () => {
+export const SuccessCheckoutPage = () => {
   return (
     <div className="relative flex flex-col flex-1 p-[10px]">
-      <Navbar />
+      <div className="absolute w-[calc(100%-20px)] min-h-[calc(100vh-20px)] h-[calc(100%-20px)] bg-[#060A23] -z-10 rounded-[20px]">
+        <img
+          src="/assets/images/backgrounds/background-glow.png"
+          className="absolute bottom-0 transform -translate-x-1/2 left-1/2"
+          alt=""
+        />
+      </div>
       <nav className="flex items-center justify-between p-[30px]">
-        <div className="flex items-center gap-[60px]">
-          <img
-            src="/assets/images/logos/logo.svg"
-            className="flex shrink-0"
-            alt="logo"
-          />
-          <ul className="flex items-center gap-10">
-            <li className="font-semibold transition-all duration-300 hover:text-[#662FFF] text-white">
-              <Link to="#">Home</Link>
-            </li>
-            <li className="font-semibold transition-all duration-300 hover:text-[#662FFF] text-white">
-              <Link to="pricing.html">Pricing</Link>
-            </li>
-            <li className="font-semibold transition-all duration-300 hover:text-[#662FFF] text-white">
-              <Link to="#">Features</Link>
-            </li>
-            <li className="font-semibold transition-all duration-300 hover:text-[#662FFF] text-white">
-              <Link to="#">Testimonials</Link>
-            </li>
-          </ul>
-        </div>
+        <Navbar />
         <div className="flex items-center gap-3">
-          <Link to="#">
+          <Link to="signin.html">
             <div className="flex items-center gap-3 w-fit rounded-full border p-[14px_20px] transition-all duration-300 hover:bg-[#662FFF] hover:border-[#8661EE] hover:shadow-[-10px_-6px_10px_0_#7F33FF_inset] bg-[#070B24] border-[#24283E] shadow-[-10px_-6px_10px_0_#181A35_inset]">
               <span className="font-semibold text-white">My Dashboard</span>
             </div>
           </Link>
-          <Link to="#">
+          <Link to="signup.html">
             <div className="flex items-center gap-3 w-fit rounded-full border p-[14px_20px] transition-all duration-300 hover:bg-[#662FFF] hover:border-[#8661EE] hover:shadow-[-10px_-6px_10px_0_#7F33FF_inset] bg-[#662FFF] border-[#8661EE] shadow-[-10px_-6px_10px_0_#7F33FF_inset]">
               <span className="font-semibold text-white">Sign Up</span>
             </div>
@@ -47,5 +33,3 @@ const SuccessCheckoutPage = () => {
     </div>
   );
 };
-
-export default SuccessCheckoutPage;
