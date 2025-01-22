6585c7a3b5b2e5f9e19533b6686ff950d100438d
hafidz491 <hafidzhidayatulloh491@gmail.com>
2025-01-22 22:29:39 +0700

    create students item and adding students item to page

--- src/pages/Students/index.jsx
+++ src/pages/Students/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import StudentsItem from "./students-item";
 
 export default function ManageStudetnsPage() {
   return (
@@ -32,46 +33,7 @@ export default function ManageStudetnsPage() {
         id="CourseList"
         className="flex flex-col w-full rounded-[30px] p-[30px] gap-[30px] bg-[#F8FAFB]"
       >
-        <div className="flex items-center gap-5 card">
-          <div className="relative flex w-20 h-20 shrink-0">
-            <div className="rounded-[20px] bg-[#D9D9D9] overflow-hidden">
-              <img
-                src="/assets/images/photos/photo-3.png"
-                className="object-cover w-full h-full"
-                alt="photo"
-              />
-            </div>
-          </div>
-          <div className="w-full">
-            <h3 className="font-bold text-xl leading-[30px] line-clamp-1">
-              Angga Risky Setiawan
-            </h3>
-            <div className="flex items-center gap-5">
-              <div className="flex items-center gap-[6px] mt-[6px]">
-                <img
-                  src="/assets/images/icons/note-favorite-purple.svg"
-                  className="w-5 h-5"
-                  alt="icon"
-                />
-                <p className="text-[#838C9D]">183 Course Joined</p>
-              </div>
-            </div>
-          </div>
-          <div className="flex items-center justify-end gap-3">
-            <Link
-              to="#"
-              className="w-fit rounded-full border border-[#060A23] p-[14px_20px] font-semibold text-nowrap"
-            >
-              Edit Profile
-            </Link>
-            <button
-              type="button"
-              className="w-fit rounded-full p-[14px_20px] bg-[#FF435A] font-semibold text-white text-nowrap"
-            >
-              Delete
-            </button>
-          </div>
-        </div>
+        <StudentsItem />
 
         {/* <div id="Pagination" className="flex items-center gap-3">
           <button
