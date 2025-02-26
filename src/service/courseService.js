6f4260cf62c05ba0013a35cfff688e48d5618e07
hafidz491 <hafidzhidayatulloh491@gmail.com>
2025-02-26 22:16:36 +0700

    integrate api get categories

--- src/service/courseService.js
+++ src/service/courseService.js
@@ -2,3 +2,6 @@ import { apiInstanceAuth } from "../utils/database";
 
 export const getCourses = async () =>
   apiInstanceAuth.get("/courses").then((res) => res.data);
+
+export const getCategories = async () =>
+  apiInstanceAuth.get("/categories").then((res) => res.data);
