3d558cedfed4946e28f0f6978ec5fd3aae6d99e2
hafidz491 <hafidzhidayatulloh491@gmail.com>
2025-03-10 23:37:21 +0700

    create schema to create content course

--- src/utils/zodSchema.js
+++ src/utils/zodSchema.js
@@ -21,3 +21,11 @@ export const createCourseSchema = z.object({
 export const updateCourseSchema = createCourseSchema.partial({
   thumbnail: true,
 });
+
+export const mutateContentSchema = z.object({
+  title: z.string().min(5),
+  type: z.string().min(4),
+  videoId: z.string().optional(),
+  text: z.string().optional(),
+  courseId: z.string().min(5),
+});
